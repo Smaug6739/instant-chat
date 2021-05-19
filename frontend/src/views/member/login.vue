7a4e093ece5885c23548e583400a130e76f3e813
Raphael <veauville.raphael@gmail.com>
2021-05-19 21:31:18 +0200

    Update

--- frontend/src/views/member/login.vue
+++ frontend/src/views/member/login.vue
@@ -1,31 +1,32 @@
 <template>
   <div id="login">
-    <div class="header__bg"></div>
     <div id="container">
       <form>
         <h4>Connexion à votre compte</h4>
-        <label for="form-username">Pseudo :</label>
-        <input
-          type="text"
-          id="form-username"
-          class="form-control mb-3"
-          placeholder="Pseudo"
-          required
-          autofocus
-        />
-        <label for="form-password">Mot de passe :</label>
-        <input
-          type="password"
-          id="form-password"
-          class="form-control mb-3"
-          placeholder="Password"
-          required
-          name="pass"
-        />
+        <div class="user-input-wrp">
+          <br />
+          <input
+            type="text"
+            id="form-username"
+            class="mb-3 form-control inputText"
+            required
+          />
+          <span class="floating-label">Your username</span>
+        </div>
+        <div class="user-input-wrp">
+          <br />
+          <input
+            type="password"
+            id="form-password"
+            class="mb-3 form-control inputText"
+            required
+          />
+          <span class="floating-label">Your password</span>
+        </div>
         <div id="error"></div>
         <div class="btns">
           <button type="button" class="btn btn-success" @click="connect">
-            Se connecter
+            Login
           </button>
           <router-link to="/member/register"
             >You don't have an account ?</router-link
@@ -76,14 +77,14 @@ export default {
   position: relative;
 }
 #container {
-  background-color: $background-element-light;
+  //background-color: $background-element-light;
   width: 45%;
   display: block;
-  border: 1px groove white;
-  border-radius: 5px;
-  padding: 0 2em 2em 2em;
-  -webkit-box-shadow: 2px 2px 2px 2px black;
-  box-shadow: 2px 2px 2px 2px black;
+  // border: 1px groove white;
+  // border-radius: 5px;
+  // padding: 0 2em 2em 2em;
+  // -webkit-box-shadow: 2px 2px 2px 2px black;
+  // box-shadow: 2px 2px 2px 2px black;
   margin: auto;
   margin-top: 8%;
 }
@@ -92,5 +93,39 @@ export default {
   a {
     margin: 10px 10px 10px 0px;
   }
+}
+.floating-label {
+  color: #757575;
+  font-weight: 500;
+  font-size: 1.1rem;
+}
+.user-input-wrp {
+  position: relative;
+  width: 100%;
+}
+.user-input-wrp .inputText {
+  width: 100%;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #777;
+  box-shadow: none !important;
+}
+.user-input-wrp .inputText:focus {
+  border-color: blue;
+  border-width: medium medium 2px;
+}
+.user-input-wrp .floating-label {
+  position: absolute;
+  pointer-events: none;
+  top: 26px;
+  left: 10px;
+  transition: 0.2s ease all;
+}
+.user-input-wrp input:focus ~ .floating-label,
+.user-input-wrp input:not(:focus):valid ~ .floating-label {
+  top: 0px;
+  left: 10px;
+  font-size: 13px;
+  opacity: 1;
 }
 </style>
